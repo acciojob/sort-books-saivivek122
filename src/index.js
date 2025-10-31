17adf36d40c93a5a860b4e74ac1765bb816316cf
Sai Vivek <saivivekbunny69@gmail.com>
2025-10-31 06:35:51 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import App from "./App";
+import "./index.css";
 
-
-
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,22 @@
-const path = require("path");
-const HtmlWebpackPlugin= require('html-webpack-plugin');
-module.exports = {
-    entry: './src/index.js',
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-    output: {
-        path: path.join(__dirname,"/dist"),
-        filename: "index_bundle.js",
-    },
-    module:{
-        rules: [
-            {
-                test: /\.js$|\.jsx$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: 'style-loader',
-                    },
-                    {
-                        loader: 'css-loader',
-                    },
-                ]
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/index.html"
-        })
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: __dirname + "/dist",
+    filename: "bundle.js",
+    publicPath: "/",
+  },
+  module: {
+    rules: [
+      { test: /\.(js|jsx)$/, exclude: /node_modules/, use: "babel-loader" },
+      { test: /\.css$/i, use: ["style-loader", "css-loader"] },
     ],
-    resolve: {
-        extensions: ['.js', '.jsx'],
-    }
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./public/index.html" })],
+  devServer: {
+    port: 8080,
+    historyApiFallback: true,
+  },
+  resolve: { extensions: [".js", ".jsx"] },
 };
